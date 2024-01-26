12fd0c6eddea997fe3e3b94950461b7b3c62efce
Adam Elaoumari <adam@admlbs.fr>
2024-01-26 17:16:29 +0100

    fix; Fixed appdeletage declarations

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,9 +4,6 @@ import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  WorkmanagerPlugin.registerTask(withIdentifier: "check-notifications-task")
-  WorkmanagerPlugin.registerTask(withIdentifier: "check-connection-task")
-  WorkmanagerPlugin.registerTask(withIdentifier: "check-events-task")
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -14,6 +11,9 @@ import workmanager
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
+    WorkmanagerPlugin.registerTask(withIdentifier: "check-notifications-task")
+    WorkmanagerPlugin.registerTask(withIdentifier: "check-connection-task")
+    WorkmanagerPlugin.registerTask(withIdentifier: "check-events-task")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
